cf59c5fff444238ffad7345cff461b5d2cac2548
Awilda <awildacantres@gmail.com>
2015-04-19 15:00:31 -0400

    Update

--- FizzBuzz/js/app.js
+++ FizzBuzz/js/app.js
@@ -1,31 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('#go').click(function() {
-      /*var toAdd = $("input[name=additems]").val();*/
-       	/*$('#items').append("<p class='items'>"+toAdd+"</p>");*/
-	var i, num;
+      
+		var num;
 
-    	var fizzBuzz = function(i) {
     		num=$("#text-box").val();
-    		i=+num;
-    		if (i < 101 && i >= 1 && i % 1===0){
-    	/*$('.list').empty();*/
-			for(i=1; i<=100; i++){
-    			if(i%15===0){
-        			$('.items').append("<p class='items'>'FizzBuzz'</p>");
+    		
+    		if (num < 101 && num >= 1 && num % 1===0){
+				for(i=1; i<=num; i++){
+    				if(i%15===0){
+        				$('#items').append("<p class='items'>'FizzBuzz'</p>");
+    				}
+    				else if(i%3===0){
+    					$('#items').append("<p class='items'>'Fizz'</p>");
+					}
+					else if(i%5===0){
+    					$('#items').append("<p class='items'>'Buzz'</p>");
+					}
+					else{
+    					$('#items').append("<p class='items'>" + i + "</p>");
+    				}
+    				if(i>=101){
+    				alert("A number from 1-100, please!");
+    				}
     			}
-    			else if(i%3===0){
-    				$('.items').append("<p class='items'>'Fizz'</p>");
-				}
-				else if(i%5===0){
-    				$('.items').append("<p class='items'>'Buzz'</p>");
-				}
-				else{
-    				$('.items').append("<p class='items'> + i + </p>");
-    			}
-    		}
 			}
-		});
 	});
+
+$("#text-box").val();
+
+
 });
-$(#text-box).val();
+
 
